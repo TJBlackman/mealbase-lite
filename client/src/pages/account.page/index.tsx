d5865089fc511b67adcc725bb2c60a69a39ffdc4
Trevor Blackman <TJBlackman08@gmail.com>
2020-08-26 10:06:46 -0600

    feat: edit email and password via website UI

---
+++ client/src/pages/account.page/index.tsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import Layout from '../../layouts/app-layout';
+import { Typography } from '@material-ui/core';
+import { useLocation } from 'react-router-dom';
+import { EditEmailAddressForm } from '../../forms/edit-user-email.form';
+import { EditPasswordForm } from '../../forms/edit-user-password.form';
+
+export const AccountPage = () => {
+  const location = useLocation();
+  console.log(location);
+
+  return (
+    <Layout>
+      <Typography variant='h2'>User Account Settings</Typography>
+      <Typography variant='body1'>Edit your user settings here.</Typography>
+      <br />
+      <Typography variant='h5'>Edit Email Address</Typography>
+      <EditEmailAddressForm />
+      <br />
+      <Typography variant='h5'>Edit Password</Typography>
+      <EditPasswordForm />
+    </Layout>
+  );
+};
