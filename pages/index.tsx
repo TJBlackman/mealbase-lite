9efe1b25b7dfb16fbca5ef12b3759fe6886b7cab
unknown <trevor.blackman@eclypses.com>
2022-05-14 09:42:03 -0600

    fix: quotes

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,3 +1,8 @@
 export default function Index() {
-  return <p>Home page</p>;
+  return (
+    <p>
+      Mealbase is an app that helps you save, search, and organize your favorite
+      recipes from anywhere on the web.
+    </p>
+  );
 }
